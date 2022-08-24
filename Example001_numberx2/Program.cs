950175a6cc2774746b6e431829c47631e1c803e7
Alexander <Almomsk@gmail.com>
2022-08-24 15:09:07 +0300

    Add new project Example001_numberx2

---
+++ Example001_numberx2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+int sqr = number * number;
+Console.WriteLine("Результат = {0}", sqr);
+Console.WriteLine($"Результат = {sqr}");
+
+Console.WriteLine($"Результат = {Math.Pow(number, 2)}");
