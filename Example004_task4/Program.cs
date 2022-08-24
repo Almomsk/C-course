acd593ddf3bfdd1907453b4dc78db7d2ffa4d92f
Alexander <Almomsk@gmail.com>
2022-08-24 16:18:44 +0300

    Add new projects Example005_task5 and Example004_task4

---
+++ Example004_task4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Add number: ");
+int num = int.Parse(Console.ReadLine());
+int current = -num;
+Console.WriteLine(current);
+while(current <= num)
+{
+    Console.Write($"{current} ");
+    current++;
+}
---
+++ Example005_task5/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Add new number: ");
+int num = int.Parse(Console.ReadLine());
+int result = (num / 10) % 10;
+Console.WriteLine(result);
