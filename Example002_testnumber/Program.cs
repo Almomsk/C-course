1e669e17708705e721478969b082bd61d359dfc2
Alexander <Almomsk@gmail.com>
2022-08-24 15:39:08 +0300

    Add new projects Example001_numberx2 and Example003_task3

---
+++ Example002_testnumber/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+double sqr_b = Math.Pow (b, 2);
+if (a == sqr_b)
+{
+Console.WriteLine("Yes");
+}
+else
+{
+Console.WriteLine("No");
+} 
---
+++ Example003_task3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter day of the week");
+int day = int.Parse(Console.ReadLine());
+
+if(0 < day && day < 8)
+{
+    if (day == 1) Console.WriteLine("Monday");
+    else if (day == 2) Console.WriteLine("Tuesday");
+    else if (day == 3) Console.WriteLine("Wednesday");
+    else if (day == 4) Console.WriteLine("Thursday");
+    else if (day == 5) Console.WriteLine("Friday");
+    else if (day == 6) Console.WriteLine("Saturday");
+    else if (day == 7) Console.WriteLine("Sunday");
+}
+else
+{
+    Console.WriteLine("Not right day of the week");
+}
