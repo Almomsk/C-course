dbb21a2fa70825c5efc5350d34f5e0b4485451a6
Alexander <Almomsk@gmail.com>
2022-08-31 00:43:24 +0300

    add new projects as homework - task25,27,29,star

---
+++ Task_star_seminar_4/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача “со звездочкой”: Напишите функцию, которая принимает одно число - высоту елочки и рисует ее в консоли звездочками.
+// 5 ->
+
+void Elka(int number)
+{
+  Console.WriteLine($"{number} -> ");
+  for (int i = 0; i < number; i++)
+  {
+    for (int p = number; p > i; p--)
+      {
+        Console.Write(" ");
+      }
+    for (int j = 0; j <= i * 2; j++)
+    {
+      Console.Write("*");
+    }
+  Console.WriteLine();
+  }
+
+
+
+}
+
+int Check()
+{
+    int number;
+    while (true)
+    {
+        Console.Write($"Введите число: ");
+        string str = Console.ReadLine();
+
+        try
+        {
+          number = Convert.ToInt32(str);
+          return number;
+        }
+        catch (Exception)
+        {
+          Console.WriteLine("Ошибка, введите другое число");
+        }
+    }
+}
+
+
+int number = Check();
+Console.WriteLine();
+Elka(number);
