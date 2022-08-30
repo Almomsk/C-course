d51693091313085f0c4c787df0b6694012ccc3f5
Alexander <Almomsk@gmail.com>
2022-08-30 14:46:36 +0300

    Add new projects

---
+++ Task12/Program.cs
@@ -0,0 +1,20 @@
+﻿// 12. Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.​
+// 34, 5 -> не кратно, остаток 4 ​
+// 16, 4 -> кратно
+
+Console.WriteLine("Add first number: ");
+int a1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Add second number: ");
+int a2 = int.Parse(Console.ReadLine());
+
+int a3 = a1 % a2;
+
+if(a3 == 0)
+{
+    Console.WriteLine("divisible");
+}
+else
+{
+    Console.WriteLine($"not divisible. End The remainder is equal to = {a3}");
+}
---
+++ Task16/Program.cs
@@ -0,0 +1,23 @@
+﻿// 16. Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого. 
+// 5, 25  ->  да​
+//-4, 16  ->  да​
+// 25, 5  ->  да​
+// 8,9  ->  нет
+
+
+Console.WriteLine("Add first number: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Add second number: ");
+int num2 = int.Parse(Console.ReadLine());
+
+// if (num1 * num1 == 0 || num2 * num2 == 0);
+
+if(num1 * num1 == num2 || num2 * num2 == num1)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
