65374bcc0672cb68effa7a56f5520080053f875a
Alexander <Almomsk@gmail.com>
2022-08-30 15:35:06 +0300

    Add new projects

---
+++ Task13_homework_seminar_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру (справа налево) заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 6​
+// 78 -> третьей цифры нет​
+// 32679 -> 6
+
+Console.Write("Введи число: ");
+int number = int.Parse(Console.ReadLine());
+string numberText = Convert.ToString(number);
+if (numberText.Length > 2)
+{
+  Console.WriteLine("третья цифра: " + numberText[2]);
+}
+else 
+{
+  Console.WriteLine("третьей цифры нет");
+}
---
+++ Task15_homework_seminar_2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да ​
+// 7 -> да ​
+// 1 -> нет
+
+int dayNumber = ReadInt("Введите число от 1 до 7: ");
+Console.WriteLine(WorkHoliday(dayNumber));
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+string WorkHoliday(int a)
+{
+    if (a > 0 && a < 8)
+    {
+        if (a == 7 || a == 6)
+        {
+            Console.Write(a + " - Выходной день");
+        }
+        else
+        {
+            Console.Write(a + " - Рабочий день");
+        }
+    }
+    else
+    {
+        Console.Write("Число не является днем недели");
+    }
+    return " день.";
+}
